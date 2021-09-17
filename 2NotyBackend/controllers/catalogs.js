b2b9d23d33a6d99d171694d7ea2c384f4295e247
hugo <ing.hugo.cruz@outlook.es>
2021-09-17 12:29:21 -0500

    📝 added update and create catalogs

--- 2NotyBackend/controllers/catalogs.js
+++ 2NotyBackend/controllers/catalogs.js
@@ -1,71 +1,263 @@
 const { response } = require("express");
+const { param } = require("express-validator");
 const { pool } = require("./../dbCongif");
 const getStatus = async (req, res = response) => {
-    pool.connect().then((client) => {
-        return client
-          .query(
-            `SELECT * FROM estatus `,
-          )
-          .then((response) => {
-            client.release();
-            res.status(200).json({
-              ok: true,
-              data: response.rows,
-            });
-          })
-          .catch((err) => {
-            client.release();
-            res.status(400).json({
-              ok: false,
-              msg: err,
-            });
-          });
+  pool.connect().then((client) => {
+    return client
+      .query(`SELECT * FROM estatus `)
+      .then((response) => {
+        client.release();
+        res.status(200).json({
+          ok: true,
+          data: response.rows,
+        });
+      })
+      .catch((err) => {
+        client.release();
+        res.status(400).json({
+          ok: false,
+          msg: err,
+        });
       });
+  });
 };
 
 const getCountries = async (req, res = response) => {
-    pool.connect().then((client) => {
-        return client
-          .query(
-            `SELECT * FROM paises `,
-          )
-          .then((response) => {
-            client.release();
-            res.status(200).json({
-              ok: true,
-              data: response.rows,
-            });
-          })
-          .catch((err) => {
-            client.release();
-            res.status(400).json({
-              ok: false,
-              msg: err,
-            });
-          });
+  pool.connect().then((client) => {
+    return client
+      .query(`SELECT * FROM paises `)
+      .then((response) => {
+        client.release();
+        res.status(200).json({
+          ok: true,
+          data: response.rows,
+        });
+      })
+      .catch((err) => {
+        client.release();
+        res.status(400).json({
+          ok: false,
+          msg: err,
+        });
       });
+  });
 };
 const getStates = async (req, res = response) => {
-    pool.connect().then((client) => {
-        return client
-          .query(
-            `SELECT * FROM estados_provincias`,
-          )
-          .then((response) => {
-            client.release();
-            res.status(200).json({
-              ok: true,
-              data: response.rows,
-            });
-          })
-          .catch((err) => {
-            client.release();
-            res.status(400).json({
-              ok: false,
-              msg: err,
-            });
-          });
+  pool.connect().then((client) => {
+    return client
+      .query(`SELECT * FROM estados_provincias`)
+      .then((response) => {
+        client.release();
+        res.status(200).json({
+          ok: true,
+          data: response.rows,
+        });
+      })
+      .catch((err) => {
+        client.release();
+        res.status(400).json({
+          ok: false,
+          msg: err,
+        });
       });
+  });
 };
 
-module.exports={getStatus,getCountries,getStates}
+const updateState = async (req, res = response) => {
+  const { id_pais, id_estatus, estado_provincia, id_estado } = req.body;
+  const estadoUpperCase = estado_provincia.toUpperCase();
+  pool.connect().then((client) => {
+    return client
+      .query(
+        `UPDATE estados_provincias SET id_estatus=$2, id_pais=$3, estado_provincia=$4 where id_estado=$1`,
+        [id_estado, id_estatus, id_pais, estadoUpperCase]
+      )
+      .then((response) => {
+        client.release();
+        res.status(200).json({
+          ok: true,
+          data: response.command,
+        });
+      })
+      .catch((err) => {
+        client.release();
+        res.status(400).json({
+          ok: false,
+          msg: err,
+        });
+      });
+  });
+};
+
+const updateCountry = async (req, res = response) => {
+  const { idPais, idEstatus, pais } = req.body;
+  const countryUpper = pais.toUpperCase();
+  pool.connect().then((client) => {
+    return client
+      .query(`UPDATE paises SET id_estatus=$2, pais=$3 where id_pais=$1`, [
+        idPais,
+        idEstatus,
+        countryUpper,
+      ])
+      .then((response) => {
+        client.release();
+        res.status(200).json({
+          ok: true,
+          data: response.command,
+        });
+      })
+      .catch((err) => {
+        client.release();
+        res.status(400).json({
+          ok: false,
+          msg: err,
+        });
+      });
+  });
+};
+const activateCountry = async (req, res = response) => {
+  const idCountry = req.params.id;
+  pool.connect().then((client) => {
+    return client
+      .query(`UPDATE paises SET id_estatus=1 where id_pais=$1`, [idCountry])
+      .then((response) => {
+        client.release();
+        res.status(200).json({
+          ok: true,
+          data: response.command,
+        });
+      })
+      .catch((err) => {
+        client.release();
+        res.status(400).json({
+          ok: false,
+          msg: err,
+        });
+      });
+  });
+};
+const activateState = async (req, res = response) => {
+  const idState = req.params.id;
+  pool.connect().then((client) => {
+    return client
+      .query(`UPDATE estados_provincias SET id_estatus=1 where id_estado=$1`, [
+        idState,
+      ])
+      .then((response) => {
+        client.release();
+        res.status(200).json({
+          ok: true,
+          data: response.command,
+        });
+      })
+      .catch((err) => {
+        client.release();
+        res.status(400).json({
+          ok: false,
+          msg: err,
+        });
+      });
+  });
+};
+const postStates = async (req, res = response) => {
+  const { id_country, state, id_status } = req.body;
+  const stateUpperCase = state.toUpperCase();
+  pool.connect().then((client) => {
+    return client
+      .query(`SELECT * FROM estados_provincias WHERE estado_provincia = $1`, [
+        stateUpperCase,
+      ])
+      .then((response) => {
+        if (response.rowCount > 0) {
+          res.status(200).json({
+            ok: true,
+            msg: "Ya se encuentra el estado dentro de base de datos",
+          });
+        } else {
+          return client
+            .query(
+              `INSERT INTO estados_provincias(id_pais,estado_provincia,id_estatus) VALUES($1,$2,$3)`,
+              [id_country, stateUpperCase,id_status]
+            )
+            .then((response) => {
+              client.release();
+              res.status(201).json({
+                ok: true,
+                msg: response.command,
+              });
+            })
+            .catch((err) => {
+              client.release();
+              res.status(400).json({
+                ok: false,
+                msg: err,
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        console.log("error 2", err);
+        client.release();
+        res.status(400).json({
+          ok: false,
+          msg: err,
+        });
+      });
+  });
+};
+const postContry = async (req, res = response) => {
+  const { country } = req.body;
+  const countryLowCase = country.toUpperCase();
+  pool.connect().then((client) => {
+    return client
+      .query(`SELECT * FROM PAISES WHERE pais=$1`, [countryLowCase])
+      .then((response) => {
+        if (response.rows.length > 0) {
+          res.status(200).json({
+            ok: true,
+            msg: "Ya se encuentra el pais dentro de base de datos",
+          });
+        } else {
+          return client
+            .query(`INSERT INTO paises(pais,id_estatus) VALUES($1,$2)`, [
+              countryLowCase,
+              1,
+            ])
+            .then((response) => {
+              client.release();
+              res.status(201).json({
+                ok: true,
+                msg: response.command,
+              });
+            })
+            .catch((err) => {
+              console.log("error");
+              client.release();
+              res.status(400).json({
+                ok: false,
+                msg: err,
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        client.release();
+        res.status(400).json({
+          ok: false,
+          msg: err,
+        });
+      });
+  });
+};
+
+module.exports = {
+  getStatus,
+  getCountries,
+  getStates,
+  postStates,
+  postContry,
+  updateState,
+  updateCountry,
+  activateCountry,
+  activateState,
+};
--- 2NotyBackend/routes/catalogs.js
+++ 2NotyBackend/routes/catalogs.js
@@ -2,10 +2,38 @@
 Rutas de usuarios Auth
 host + /api/catalogs
  */
-const router = require('express').Router();
-const {getStatus,getCountries,getStates} =require('../controllers/catalogs');
-router.get('/getStatus',getStatus);
-router.get('/getCountries',getCountries);
-router.get('/getStates',getStates);
+const { check } = require("express-validator");
+const router = require("express").Router();
+const {
+  getStatus,
+  getCountries,
+  getStates,
+  postStates,
+  postContry,
+  updateCountry,
+  updateState,
+  activateCountry,
+  activateState,
+} = require("../controllers/catalogs");
+router.get("/getStatus", getStatus);
+router.get("/getCountries", getCountries);
+router.get("/getStates", getStates);
+router.post(
+  "/state",
+  [
+    check("state", "El estado es obligatorio").not().isEmpty(),
+    check("idCountry", "El pais es obligatorio").not().isEmpty(),
+  ],
+  postStates
+);
+router.post(
+  "/country",
+  [check("country", "El pais es obligatorio").not().isEmpty()],
+  postContry
+);
+router.put("/updateCountry", updateCountry);
+router.put("/updateState", updateState);
+/* router.put("/activateCountry/:id", activateCountry);
+router.put("/activateState/:id", activateState); */
 
-module.exports= router;
+module.exports = router;
