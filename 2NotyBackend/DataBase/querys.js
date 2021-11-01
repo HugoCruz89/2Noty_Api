d0e74579810c4bdcd1f3c8373fbf3ff2668c1ddf
hugo <ing.hugo.cruz@outlook.es>
2021-11-01 14:32:42 -0600

    ✨ added insert token and update functions

--- 2NotyBackend/DataBase/querys.js
+++ 2NotyBackend/DataBase/querys.js
@@ -95,25 +95,15 @@ const insertSubscription = async (data, pathImage, pathIcono) => {
   return databaseResponse;
 };
 
-const insertPropiedadesSuscripcion=async(data, idSubscription)=>{
-
-const {label,hidden,required,editable,type,name}=data
-const databaseResponse = await pool.connect().then((client) => {
+const insertPropiedadesSuscripcion = async (data, idSubscription) => {
+  const { label, hidden, required, editable, type, name } = data;
+  const databaseResponse = await pool.connect().then((client) => {
     return client
       .query(
         `INSERT INTO public.propiedades_suscripcion(
             label, type, regex, hidden, id_suscripcion,required,editable,name)
             VALUES ( $1, $2, $3, $4, $5, $6, $7, $8);`,
-        [
-            label,
-            type,
-          "",
-          hidden,
-          idSubscription,
-          required,
-          editable,
-          name
-        ]
+        [label, type, "", hidden, idSubscription, required, editable, name]
       )
       .then((response) => {
         client.release();
@@ -131,6 +121,99 @@ const databaseResponse = await pool.connect().then((client) => {
       });
   });
   return databaseResponse;
-}
+};
 
-module.exports = { existSubscrition, insertSubscription,insertPropiedadesSuscripcion };
+const insertTokenToPushNotification = async (idTItular, platform, token) => {
+  const databaseResponse = await pool.connect().then((client) => {
+    return client
+      .query(
+        `INSERT INTO public.token_notificaciones(
+            id_usuario, token, plataforma)
+            VALUES ( $1, $2, $3);`,
+        [idTItular, token, platform]
+      )
+      .then((response) => {
+        client.release();
+        return {
+          ok: true,
+          msg: response.command,
+        };
+      })
+      .catch((err) => {
+        client.release();
+        return {
+          ok: false,
+          msg: err,
+        };
+      });
+  });
+  return databaseResponse;
+};
+
+const updateTokenToPushNotification = async (idTItular, platform, token) => {
+  const databaseResponse = await pool.connect().then((client) => {
+    return client
+      .query(
+        `UPDATE public.token_notificaciones SET token=$3, plataforma=$2 WHERE id_usuario=$1`,
+        [idTItular, platform, token]
+      )
+      .then((response) => {
+        client.release();
+        return {
+          ok: true,
+          msg: response.command,
+        };
+      })
+      .catch((err) => {
+        client.release();
+        return {
+          ok: false,
+          msg: err,
+        };
+      });
+  });
+  return databaseResponse;
+};
+
+const existTokenNotification = async (idTItular) => {
+  const databaseResponse = await pool.connect().then((client) => {
+    return client
+      .query(`SELECT * FROM token_notificaciones WHERE id_usuario=$1`, [
+        idTItular,
+      ])
+      .then((response) => {
+        client.release();
+        if (response.rows.length > 0) {
+          const obj = {
+            ok: true,
+            msg: "Ya se encuentra en BD la token",
+            id_suscripcion: response.rows[0].id_usuario,
+          };
+          return obj;
+        } else {
+          const obj = {
+            ok: false,
+            msg: "No se encontro ningua suscripcion",
+          };
+          return obj;
+        }
+      })
+      .catch((err) => {
+        client.release();
+        return {
+          ok: false,
+          msg: err,
+        };
+      });
+  });
+  return databaseResponse;
+};
+
+module.exports = {
+  existSubscrition,
+  insertSubscription,
+  insertPropiedadesSuscripcion,
+  insertTokenToPushNotification,
+  existTokenNotification,
+  updateTokenToPushNotification,
+};
